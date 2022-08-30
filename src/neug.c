05955048471a680947b271231ff18ee61d1fb07b
NIIBE Yutaka <gniibe@fsij.org>
2022-08-30 16:32:31 +0900

    ifdef-out ARM specific code in NeuG.
    Signed-off-by: NIIBE Yutaka <gniibe@fsij.org>

--- src/neug.c
+++ src/neug.c
@@ -143,10 +143,19 @@ crc32_rv_get (void)
 uint32_t
 rbit (uint32_t v)
 {
+#ifdef __ARM_ARCH
   uint32_t r;
 
   asm ("rbit	%0, %1" : "=r" (r) : "r" (v));
   return r;
+#else
+  v = ((v >> 1) & 0x55555555) | ((v & 0x55555555) << 1);
+  v = ((v >> 2) & 0x33333333) | ((v & 0x33333333) << 2);
+  v = ((v >> 4) & 0x0F0F0F0F) | ((v & 0x0F0F0F0F) << 4);
+  v = ((v >> 8) & 0x00FF00FF) | ((v & 0x00FF00FF) << 8);
+  v = ( v >> 16             ) | ( v               << 16);
+  return v;
+#endif
 }
 
 void
